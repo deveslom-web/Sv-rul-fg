846bdf886c1ef8b91b7ef89fb8106d13c1378bfb
deveslom-web <deveslom@gmail.com>
2026-04-23 07:55:04 +0600

    Update password for regions BR, US, SAC, NA

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def get_account_credentials(region: str) -> str:
     if r == "IND":
         return "uid=4421504713&password=JOBAYAR_CODX-64IGDYZCD"
     elif r in {"BR", "US", "SAC", "NA"}:
-        return "uid=4044223479&password=EB067625F1E2CB705C7561747A46D502480DC5D41497F4C90F3FDBC73B8082ED"
+        return "uid=4583733541&password=EB067625F1E2CB705C7561747A46D502480DC5D41497F4C90F3FDBC73B8082ED"
     else:
         return "uid=4660781076&password=jshsjs_T7BNL_BY_SPIDEERIO_GAMING_2R92G"
 
